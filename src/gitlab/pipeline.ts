877c456448fc022e4ec7c4d56e4470990aad3a29
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-07-05 08:43:23 +0000

    add jobs to the pipeline

--- src/gitlab/pipeline.ts
+++ src/gitlab/pipeline.ts
@@ -1,5 +1,11 @@
 import { GitlabCI } from "https://deno.land/x/fluent_gitlab_ci@v0.3.2/mod.ts";
+import { archiveProject, buildProject } from "./jobs.ts";
 
-const gitlabci = new GitlabCI();
+const gitlabci = new GitlabCI()
+  .addJob("build_project", buildProject("ProjectName", "SchemeName"))
+  .addJob(
+    "archive_project",
+    archiveProject("ProjectName", "SchemeName", "ProvisioningProfileName")
+  );
 
 export default gitlabci;

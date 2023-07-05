ead7b4af96c29e9abad4b9929046ab1c1c813a8d
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-07-05 08:25:57 +0000

    Initial Commit

---
+++ src/gitlab/index.ts
@@ -0,0 +1,4 @@
+import pipeline from "./pipeline.ts";
+import { buildProject, archiveProject } from "./jobs.ts";
+
+export { pipeline, buildProject, archiveProject };
---
+++ src/gitlab/pipeline.ts
@@ -0,0 +1,5 @@
+import { GitlabCI } from "https://deno.land/x/fluent_gitlab_ci@v0.3.2/mod.ts";
+
+const gitlabci = new GitlabCI();
+
+export default gitlabci;
